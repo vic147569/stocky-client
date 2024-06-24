6a6bd55cd1d93bee43fe47e75c5acea1d40e643b
CHIA HONG CHANG <vic147569az@gmail.com>
2024-06-24 20:09:54 +0800

    feat(app footer): finish app footer

--- src/Components/AppFooter.tsx
+++ src/Components/AppFooter.tsx
@@ -1,2 +1,12 @@
-const AppFooter = () => <div className="bg-sky-700 py-6 text-3xl">AppFooter</div>;
+import { Link } from 'react-router-dom';
+
+const AppFooter = () => (
+  <div className="bg-sky-700 py-6">
+    <div className=" container mx-auto">
+      <Link to="/" className=" text-3xl text-yellow-500 tracking-tighter font-bold">
+        Stocky
+      </Link>
+    </div>
+  </div>
+);
 export default AppFooter;
