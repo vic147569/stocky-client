335d23449b37de7c60837c8ebb233618bbcb8736
CHIA HONG CHANG <vic147569az@gmail.com>
2024-07-16 11:22:36 +0800

    ci: test deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './global.css';
-import { Suspense } from 'react';
+// import { Suspense } from 'react';
 import router from './Routes/routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Suspense fallback={<div>Loading</div>}>
-    <RouterProvider router={router} />
-  </Suspense>,
+  // <Suspense fallback={<div>Loading</div>}>
+  <RouterProvider router={router} />,
+  // </Suspense>,
 );
