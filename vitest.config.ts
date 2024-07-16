654c5fad34c40dfb4b7c66d8e5194cbafd33f934
CHIA HONG CHANG <vic147569az@gmail.com>
2024-07-16 09:18:21 +0800

    ci: test deploy

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ import viteConfig from './vite.config';
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base: '/stocky-client/',
     test: {
       root: './',
       globals: true,
