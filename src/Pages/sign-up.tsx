9ee7d0515a58831f477cb53eea98f35cdaee7f9d
CHIA HONG CHANG <vic147569az@gmail.com>
2024-07-16 16:50:03 +0800

    ci: test

--- src/Pages/sign-up.tsx
+++ src/Pages/sign-up.tsx
@@ -2,7 +2,7 @@ import { SignUp } from '@clerk/clerk-react';
 
 const SignUpPage = () => (
   <div className="flex justify-around">
-    <SignUp fallbackRedirectUrl={`${import.meta.env.VITE_CLERK_SIGN_UP_FORCE_REDIRECT_URL}auth-callback`} />
+    <SignUp fallbackRedirectUrl={`${import.meta.env.VITE_CLERK_SIGN_UP_FORCE_REDIRECT_URL}/auth-callback`} />
   </div>
 );
 
