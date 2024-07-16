4c1644931bc8ad7806c83b8171318b89d7e57ce7
CHIA HONG CHANG <vic147569az@gmail.com>
2024-07-16 10:56:56 +0800

    fix: addd lazy load

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './global.css';
+import { Suspense } from 'react';
 import router from './Routes/routes';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Suspense fallback={<div>Loading</div>}>
+    <RouterProvider router={router} />
+  </Suspense>,
+);
