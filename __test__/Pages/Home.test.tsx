1aeb0dca139c8991d209663fa439a330e8f79a97
CHIA HONG CHANG <vic147569az@gmail.com>
2024-07-16 20:59:38 +0800

    chore: finish

--- __test__/Pages/Home.test.tsx
+++ __test__/Pages/Home.test.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { useNavigate } from 'react-router-dom';
